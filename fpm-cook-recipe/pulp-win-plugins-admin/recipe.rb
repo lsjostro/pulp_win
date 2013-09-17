22fce104c7fe997b26c8b9d1e37ad58838a0794d
Lars Sjöström <lars.sjostrom@svenskaspel.se>
2013-09-17 17:58:54 +0200

    separate admin and server plugn

--- fpm-cook-recipe/pulp-win-plugins-admin/recipe.rb
+++ fpm-cook-recipe/pulp-win-plugins-admin/recipe.rb
@@ -1,6 +1,6 @@
 class Pulp_win < FPM::Cookery::Recipe
-  description 'Pulp Windows plugins'
-  name        'pulp-win-plugins'
+  description 'Pulp Windows plugins admin extension'
+  name        'pulp-win-plugins-admin'
   version     '0.1'
   revision    '1'
   homepage    'https://github.com/lsjostro/pulp_win/'
@@ -20,11 +20,6 @@ class Pulp_win < FPM::Cookery::Recipe
 
   def install
     lib('python2.6/site-packages').install_p 'pulp_win'
-    lib('pulp/plugins/').install_p 'importers'
-    lib('pulp/plugins/').install_p 'distributors'
-    lib('pulp/plugins/').install_p 'types' 
     lib('pulp/').install_p 'admin'
-    etc('httpd/conf.d').install 'pulp_win.conf'
-    var('www/pulp_win/http/repos').mkpath
   end
 end
---
+++ fpm-cook-recipe/pulp-win-plugins-server/recipe.rb
@@ -0,0 +1,28 @@
+class Pulp_win < FPM::Cookery::Recipe
+  description 'Pulp Windows plugins'
+  name        'pulp-win-plugins-server'
+  version     '0.1'
+  revision    '1'
+  homepage    'https://github.com/lsjostro/pulp_win/'
+  source      'https://github.com/lsjostro/pulp_win.git', :with => :git
+  arch        'noarch'
+  section     'Development/Languages'
+
+  #python= [
+  #    "python-sh",
+  #    "msitools",
+  #]
+
+  #depends python
+
+  def build
+  end
+
+  def install
+    lib('pulp/plugins/').install_p 'importers'
+    lib('pulp/plugins/').install_p 'distributors'
+    lib('pulp/plugins/').install_p 'types' 
+    etc('httpd/conf.d').install 'pulp_win.conf'
+    var('www/pulp_win/http/repos').mkpath
+  end
+end
